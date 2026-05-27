0db3f88c0fe2a02341df32688fc357a2914507b8
juanjosesalazarlopez29-spec <juanjosesalazarlopez29@gmail.com>
2026-05-26 22:25:21 -0500

    Add files via upload

---
+++ Informe #1/#ejercicio vocales.py
@@ -0,0 +1,7 @@
+#ejercicio vocaltes
+vocales = "a,e,i,o,u,A,E,I,O,U"
+letra = input("Ingrese una letra: ")
+
+if letra in vocales: 
+    print("La letra es una vocal.")
+else:   print("La letra no es una vocal.")
