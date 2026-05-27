33d4f48df77d08b42fa951ca85cf03db685a7ef0
juanjosesalazarlopez29-spec <juanjosesalazarlopez29@gmail.com>
2026-05-26 23:02:58 -0500

    Delete Informe #1 directory

--- Informe #1/#ejercicio vocales.py
+++
@@ -1,7 +0,0 @@
-#ejercicio vocaltes
-vocales = "a,e,i,o,u,A,E,I,O,U"
-letra = input("Ingrese una letra: ")
-
-if letra in vocales: 
-    print("La letra es una vocal.")
-else:   print("La letra no es una vocal.")
